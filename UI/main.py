81672990d7a10f4623e32cfeb85fd550f99d5ae4
MiyazawaYoshiki <nozawanarain@yahoo.co.jp>
2019-01-07 05:30:17 +0900

    画面遷移を実装
    起動画面から良否登録画面へ遷移可能にした

--- UI/main.py
+++ UI/main.py
@@ -1,33 +1,42 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*
 from kivy.config import Config
 Config.set('graphics', 'width', '640')
 Config.set('graphics', 'height', '480')
+import kivy
+kivy.require('1.10.1')
 
+from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.properties import StringProperty, ListProperty
 from kivy.core.text import LabelBase, DEFAULT_FONT
+from kivy.core.window import Window
+from kivy.properties import BooleanProperty
+from kivy.utils import get_color_from_hex
 from kivy.resources import resource_add_path
 
+from kivy.factory import Factory
+
 # デフォルトに使用するフォントを変更する
-resource_add_path('./fonts')
+resource_add_path('fonts')
 LabelBase.register(DEFAULT_FONT, 'mplus-2c-regular.ttf') #日本語が使用できるように日本語フォントを指定する
 
 
-class TextWidget(Widget):
-    text  = StringProperty()
+class StartScreen(BoxLayout):
+    pass
 
+class TestRoot(BoxLayout):
     def __init__(self, **kwargs):
-        super(TextWidget, self).__init__(**kwargs)
-        pass
+        super(TestRoot, self).__init__(**kwargs)
 
-    def buttonClicked(self):
-        pass
+    def go_checkScreen(self):   
+        self.clear_widgets()
+        checkscreen = Factory.checkscreen()
+        self.add_widget(checkscreen)
 
-class TestApp(App):
+class TestApp(App): 
     def __init__(self, **kwargs):
         super(TestApp, self).__init__(**kwargs)
         self.title = '映画レコメンドシステム'
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    Window.clearcolor = get_color_from_hex('#FFFFFF')
     TestApp().run()
